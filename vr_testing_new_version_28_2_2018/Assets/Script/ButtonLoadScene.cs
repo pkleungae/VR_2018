f58782b6928bff0cf73efe8360ff5ab465798ef5
pkleungae <pkleungae@connect.ust.hk>
2018-02-28 19:39:20 +0800

    fyp

---
+++ vr_testing_new_version_28_2_2018/Assets/Script/ButtonLoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour {
+
+	
+
+    public void LoadScenetofirstScene()
+    {
+        SceneManager.LoadScene("firstScene");
+    }
+
+    public void LoadScenetohelpScene()
+    {
+        SceneManager.LoadScene("helpScene");
+    }
+
+    public void LoadScenetosearchScene()
+    {
+        SceneManager.LoadScene("searchScene");
+    }
+}
